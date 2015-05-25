22728758f09a4f9a29c0e8fd13e33a2f8f05ed61
rey <rey@live.jp>
2015-05-25 11:43:37 +0800

    Removed  Slice

--- scrape.js
+++ scrape.js
@@ -9,7 +9,7 @@ for(var j = 0; j<mainlist.length; j++)
   var curLevel = parseInt(mainlist[j].style.width);
   if(stackIndex.length == 0)
   {
-    stackIndex.push(output.slice());
+    stackIndex.push(output);
     stackLevel.push(curLevel);
   }
 
@@ -21,8 +21,9 @@ for(var j = 0; j<mainlist.length; j++)
 
   while(stackLevel[0] > curLevel)
   {
-    //stackIndex.push(stackIndex[0][stackIndex[0].length-1].slice());
+    stackIndex.push(stackIndex[0][stackIndex[0].length-1]);
     stackIndex.push(stackLevel - 20);
+    curLevel -=20;
   }
     console.log(stackIndex, stackLevel);
 
